3ceee01752de220b08ad497e39d4060045fc2048
danciuionela <58913179+danciuionela@users.noreply.github.com>
2026-07-12 19:11:00 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,46 +1,49 @@
-function generateCertificate() {
-    const name = document.getElementById("name").value.trim();
-    if(name === "") {
-        alert("Te rugăm să completezi numele 🤍");
-        return;
-    }
-    let personalities = [];
-    document
-    .querySelectorAll(".checks input:checked")
-    .forEach(item => {
-        personalities.push(item.value);
-    });
-    const prediction = {
-        name: name,
-        relation:
-        document.getElementById("relation").value,
-        date:
-        document.getElementById("month").value
-        + " "
-        + document.getElementById("day").value,
-        weight:
-        document.getElementById("weight").value,
-        height:
-        document.getElementById("height").value,
-        hair:
-        document.getElementById("hair").value,
-        eyes:
-        document.getElementById("eyes").value,
-        look:
-        document.getElementById("look").value,
-        personality:
-        personalities.join(", "),
-        word:
-        document.getElementById("word").value,
-        job:
-        document.getElementById("job").value,
-        message:
-        document.getElementById("message").value
-    };
-    localStorage.setItem(
-        "adamPrediction",
-        JSON.stringify(prediction)
-    );
-    window.location.href =
-    "certificate.html";
+function generateCertificate(){
+const name =
+document.getElementById("name").value.trim();
+if(name === ""){
+alert("Te rugăm să completezi numele 🤍");
+return;
+}
+let personalities = [];
+document
+.querySelectorAll(".checks input:checked")
+.forEach(item=>{
+personalities.push(item.value);
+});
+const prediction = {
+name:name,
+relation:
+document.getElementById("relation").value,
+date:
+document.getElementById("month").value
++
+" "
++
+document.getElementById("day").value,
+weight:
+document.getElementById("weight").value,
+height:
+document.getElementById("height").value,
+hair:
+document.getElementById("hair").value,
+eyes:
+document.getElementById("eyes").value,
+look:
+document.getElementById("look").value,
+personality:
+personalities.join(", "),
+word:
+document.getElementById("word").value,
+job:
+document.getElementById("job").value,
+message:
+document.getElementById("message").value
+};
+localStorage.setItem(
+"adamPrediction",
+JSON.stringify(prediction)
+);
+window.location.href =
+"certificate.html";
 }
