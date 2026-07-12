6e43594afaab24fc16c826852f01b63921741a2c
danciuionela <58913179+danciuionela@users.noreply.github.com>
2026-07-12 19:02:08 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,46 @@
-function download() {
-    const button = document.querySelector("button");
-
-    // Ascunde butonul înainte de captură
-    button.style.display = "none";
-
-    html2canvas(document.querySelector("#card"), {
-        scale: 3,
-        useCORS: true
-    }).then(canvas => {
-
-        const link = document.createElement("a");
-        link.download = "baby-predictions.png";
-        link.href = canvas.toDataURL("image/png");
-        link.click();
-
-        // Afișează din nou butonul
-        button.style.display = "block";
+function generateCertificate() {
+    const name = document.getElementById("name").value.trim();
+    if(name === "") {
+        alert("Te rugăm să completezi numele 🤍");
+        return;
+    }
+    let personalities = [];
+    document
+    .querySelectorAll(".checks input:checked")
+    .forEach(item => {
+        personalities.push(item.value);
     });
+    const prediction = {
+        name: name,
+        relation:
+        document.getElementById("relation").value,
+        date:
+        document.getElementById("month").value
+        + " "
+        + document.getElementById("day").value,
+        weight:
+        document.getElementById("weight").value,
+        height:
+        document.getElementById("height").value,
+        hair:
+        document.getElementById("hair").value,
+        eyes:
+        document.getElementById("eyes").value,
+        look:
+        document.getElementById("look").value,
+        personality:
+        personalities.join(", "),
+        word:
+        document.getElementById("word").value,
+        job:
+        document.getElementById("job").value,
+        message:
+        document.getElementById("message").value
+    };
+    localStorage.setItem(
+        "adamPrediction",
+        JSON.stringify(prediction)
+    );
+    window.location.href =
+    "certificate.html";
 }
